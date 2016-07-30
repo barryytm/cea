2fb2a9bc57935a33a56b6cdb0141755eef272530
njia95 <nathan.jia@mail.utoronto.ca>
2016-07-30 15:49:28 -0400

    implement course addtion

--- public/js/index.js
+++ public/js/index.js
@@ -7,24 +7,28 @@ function snack(msg) {
 }
 
 $(document).ready(() => {
-    var $username;
+    var username;
+    var courses = [];
 
     $('form').submit((event) => {
         event.preventDefault();
     });
 
     $('#infoForm').hide();
+    $('#courseForm').hide();
 
     $('#loginForm').submit(() => {
-        $username = $('#username').val();
+        username = $('#username').val();
         $('#loginForm').hide();
 
         snack('Logged in');
         
-        $.post('/login', {username: $username}, result => {
-            $('#name').text($username);
+        $.post('/login', {username: username}, result => {
+            $('#name').text(username);
             if (!result) {
                 $('#infoForm').show();
+            } else {
+                $('#courseForm').show();
             }
         });
     });
@@ -35,7 +39,7 @@ $(document).ready(() => {
         var $gender = $('#male').is(':checked') ? 'm' : 'f';
 
         var $info = {
-            username: $username, 
+            username: username, 
             age: $age, 
             country: $country, 
             gender: $gender
@@ -43,10 +47,24 @@ $(document).ready(() => {
 
         snack('Submitted');
 
+        $('#infoForm').hide();
+
         $.post('/info', $info, result => {
-
+            $('#courseForm').show();
         });
+    });
 
+    $('#courseForm').submit(() => {
+        var $course = $('#course').val();
+        if (courses.includes($course)) {
+            snack('No duplicate');
+        } else {
+            $('<li/>').text($course).appendTo('#courses');
+            courses.push($course);
+        }
+    });
+
+    $('#courses').children().click(() => {
 
     });
 });
